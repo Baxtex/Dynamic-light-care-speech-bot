485fcaffef86607f7a4547f3ccf53ab34a9e2e56
Baxtex <anton.gustafsson@protonmail.com>
2017-05-12 05:48:49 +0200

    init

---
+++ ConsoleApplication1/Apihandler.cs
@@ -0,0 +1,33 @@
+﻿using RestSharp;
+using System;
+
+namespace ConsoleApplication
+{
+    public static class ApiHandler
+    {
+        private const string baseUrl = "http://dynamiclight.azurewebsites.net/api/1/";
+        private static readonly RestClient client;
+
+        static ApiHandler()
+        {
+            client = new RestClient();
+            client.BaseUrl = new Uri(baseUrl);
+        }
+
+        //Turn light on or off.
+        internal static void PostTurnLights(string action)
+        {
+            var request = new RestRequest("http://10.2.28.129:8081/Turndeviceon", Method.POST);
+            request.AddQueryParameter("action", action);
+            var response = client.Execute(request);
+        }
+
+        //Change the ligt. 
+        internal static void PostChangeLight(string action)
+        {
+            var request = new RestRequest("http://10.2.28.129:8081/Turndeviceoff", Method.POST);
+            request.AddQueryParameter("action", action);
+            var response = client.Execute(request);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechRecognitionEngine reco = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+            reco.SetInputToDefaultAudioDevice();
+            reco.LoadGrammar(new DictationGrammar());
+            reco.RecognizeAsync(RecognizeMode.Multiple);
+            reco.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(reco_reconized);
+
+            while (true)
+            {
+                Console.ReadKey();
+            }
+        }
+
+        private static void reco_reconized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Console.WriteLine(e.Result.Text);
+            string res = e.Result.Text;
+            if (res.Contains("on"))
+            {
+                ApiHandler.PostTurnLights("true");
+            }
+
+            if (res.Contains("off"))
+            {
+                ApiHandler.PostTurnLights("false");
+            }
+        }
+    }
+
+
+}
